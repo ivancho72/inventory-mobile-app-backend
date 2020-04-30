69ebc096ca96e2364aeb8f3805cd74584c399401
ivancho72 <jorgeluna@inbox.com>
2020-04-29 20:14:09 -0400

    strapi startup try

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
 "use strict";
-// const strapi = require("./node_modules/strapi/lib/index");
-// const strapiDev = require("./node_modules/strapi/lib/commands/develop");
+const strapi = require("./node_modules/strapi/lib/index");
+const strapiDev = require("./node_modules/strapi/lib/commands/develop");
 
-// if (process.env.DEV_MODE === true) {
-//   strapiDev();
-// } else {
-//   strapi().start();
-// }
-
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(process.env.PORT || 5000, console.log("listening!"));
+if (process.env.DEV_MODE === true) {
+  strapiDev();
+} else {
+  strapi().start();
+}
